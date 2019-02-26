8c7f8eba05930457339a74b533614a15ae06892a
alunasa1 <43356585+alunasa1@users.noreply.github.com>
2019-02-26 16:28:44 -0700

    Update DetectCircles.py

--- DetectCircles.py
+++ DetectCircles.py
@@ -12,10 +12,12 @@ edges = cv2.Canny(gray, 50, 150, apertureSize = 3)
 circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100) #finds all circles based on edges
 
 if circles is not None: 
-	circleList = np.round(circles[0,:].astype("int")) #makes a list of circles
+	circleList = np.round(circles[0,:].astype("int") #makes a list of circles
 
-	for(x,y,r) in circles:
+	for (x,y,r) in circles:
 		cv2.circle(image, (x,y), r, (255,0,255), 5) #draws circle around circle with specified color and thickness on image
 
 	cv2.imwrite("circles", image)
+
+
 
