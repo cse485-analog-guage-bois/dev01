0444cc53e6740fa8d06798c29d67a005ccbd8f74
alunasa1 <43356585+alunasa1@users.noreply.github.com>
2019-03-13 01:52:35 -0700

    Update detectlines.py

--- detectlines.py
+++ detectlines.py
@@ -1,30 +1,32 @@
-# Python program to illustrate HoughLine  
 import cv2 
-import numpy as np 
-  
-# Reading the required image in  
-# which operations are to be done.  
-# Make sure that the image is in the same  
-# directory in which this python program is 
-img = cv2.imread('line.png') 
-  
-# Convert the img to grayscale 
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
-  
-# Apply edge detection method on the image 
-edges = cv2.Canny(gray,300,800,apertureSize = 3) 
-  
+import numpy as np
 
-maxLineGap = 20
-minLineLength = 70 
-lines = cv2.HoughLinesP(edges,1,np.pi/180, 40, maxLineGap, minLineLength) 
-  
 
-if lines is not None:
-    for line in lines[0]: 
+cimg = cv2.imread('gauge3.png') 
+  
+cimg = cv2.medianBlur(cimg,5)
+
+img = cv2.cvtColor(cimg, cv2.COLOR_BGR2GRAY)
+
+edges = cv2.Canny(img, 50, 150, apertureSize = 3)
+
+binary_threshold = 40
+
+#img3 = cv2.medianBlur(img,17)
+
+#ret1, img2 = cv2.threshold(img3, binary_threshold, 255, cv2.THRESH_BINARY)
+    
+#edges = cv2.Canny(img2, 30, 150, apertureSize = 3)
+        
+lines1 = cv2.HoughLinesP(edges,1,np.pi/180, 40,maxLineGap = 20, minLineLength = 70)
+
+if lines1 is not None:
+    for line in lines1:
         x1,y1,x2,y2 = line[0]
-        cv2.line(img,(x1,y1), (x2,y2), (0,255,0),2) 
-      
+        cv2.line(cimg,(x1,y1),(x2,y2),(0,255,0),2)
+        
+cv2.imwrite('edges.jpg',edges)
+#cv2.imwrite('img2.jpg',img2)
+cv2.imwrite('linedetect.jpg',cimg)
 
-cv2.imwrite('linesDetected.jpg', img) 
-cv2.imwrite('linesDetectedblur.jpg', edges) 
+
